404aff587ae931c33983cf4d7866682db86c2b5f
surajgautam <teamwarrior200@gmail.com>
2019-11-23 23:33:07 +0545

    Add rest-assured

--- service/src/test/java/com/surajgautam/demo/controller/EmployeeControllerTest.java
+++ service/src/test/java/com/surajgautam/demo/controller/EmployeeControllerTest.java
@@ -1,8 +1,13 @@
 package com.surajgautam.demo.controller;
 
+import com.surajgautam.demo.domain.Employee;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -14,6 +19,10 @@ class EmployeeControllerTest {
     @LocalServerPort
     private int port;
 
-
-
+    @Test
+    void givenValidRequest_whenGet_ThenStatusOK() {
+        Employee[] employees = when().get("http://localhost:" + port + "/api/v1/employees")
+                .then().statusCode(HttpStatus.OK.value())
+                .and().extract().as(Employee[].class);
+    }
 }
