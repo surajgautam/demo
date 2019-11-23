a962409245abd3bf96449b0956880bf9c367ce12
surajgautam <teamwarrior200@gmail.com>
2019-11-23 22:00:41 +0545

    initialize

---
+++ src/main/java/com/surajgautam/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.surajgautam.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoOperations;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner (MongoOperations mongoOperations){
+		//save
+		System.out.println("test");
+		return (args)->{};
+	};
+}
